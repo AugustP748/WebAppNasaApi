745e748261647e54655d68218396ff3013bf6ffd
Augusto <augustoxx2015@gmail.com>
2023-10-14 00:17:58 -0300

    The Home page has been enhanced, and apod looks great now

--- WebAppNasaApi/Models/Apod.cs
+++ WebAppNasaApi/Models/Apod.cs
@@ -5,5 +5,8 @@
         public string explanation { get; set; }
         public string title { get; set; }
         public string url { get; set; }
+        public string hdurl { get; set; }
+        public string copyright { get; set; }
+        public DateTime date { get; set; }
     }
 }
